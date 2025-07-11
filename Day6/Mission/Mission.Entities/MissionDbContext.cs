bb338ce06659af9dca7bdb08385830f1b147d159
Harshana6 <harshanagohil@gmail.com>
2025-07-12 03:19:19 +0530

    Added Day6 project folder

---
+++ Day6/Mission/Mission.Entities/MissionDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Models;
+
+namespace Mission.Entities
+{
+    public class MissionDbContext(DbContextOptions<MissionDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<MissionSkill> MissionSkills { get; set; }
+
+        public DbSet<MissionTheme> MissionThemes { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Harshana",
+                LastName = "Gohil",
+                EmailAddress = "admin@gmail.com",
+                Password = "Harshana",
+                PhoneNumber = "01234567890",
+                UserType = "admin",
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
